acaeecc81f466f85bca6984b8bfd1420e63057d5
Samuel Marxer <ms6903@mci4me.at>
2025-06-18 15:43:41 +0200

    Abgabe: Objektorientierung

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 from PIL import Image
-from src.ekgdata import EKGdata     
+from src.ekgdata import EKGdata
 from src.person import Person
 from src.analyze_activity_data import dataplot, dataframe
 
@@ -21,7 +21,7 @@ st.image(Image.open(selected_person.picture_path), caption=selected_name)
 # Maximalpuls-Eingabe (aus Klasse vorbelegen)
 hr_max = st.number_input("Maximale Herzfrequenz", min_value=100, max_value=250, value=int(selected_person.hr_max), step=1)
 
-# Analyse-Plot anzeigen
+# Analyse-Plot anzeigen (öffnet kein extra Fenster!)
 st.plotly_chart(dataplot(hr_max))
 
 # Zonenstatistik berechnen und anzeigen
@@ -32,11 +32,14 @@ zone_minutes.name = "Dauer (Minuten)"
 zone_power = dataframe.groupby("Zone")["PowerOriginal"].mean()
 zone_stats = zone_minutes.to_frame().join(zone_power.rename("Ø Power (W)"))
 
-
 st.write("## Zonenstatistik")
 st.dataframe(zone_stats)
+
 # Beispiel: Erstes EKG der ausgewählten Person verwenden
-ekg_obj = EKGdata(selected_person.ekg_tests[0])
-ekg_obj.find_peaks()  # Peaks berechnen
-hr_est = ekg_obj.estimate_hr()
-st.write(f"Geschätzte Herzfrequenz aus EKG: {hr_est:.1f} bpm")
+if selected_person.ekg_tests:
+    ekg_obj = EKGdata(selected_person.ekg_tests[0])
+    ekg_obj.find_peaks()
+    hr_est = ekg_obj.estimate_hr()
+    st.write(f"Geschätzte Herzfrequenz aus EKG: {hr_est:.1f} bpm")
+else:
+    st.write("Keine EKG-Daten für diese Person vorhanden.")
